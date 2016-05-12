b66f64b8cf61327b4264ad6c54acca8242e4b766
Calvin Wong <calvwng@gmail.com>
2016-05-11 21:23:29 -0700

    Still looping

--- bot.js
+++ bot.js
@@ -5,7 +5,7 @@ var botID = process.env.BOT_ID;
 
 function respond() {
   var request = JSON.parse(this.req.chunks[0]);
-  var botRegex = /^[\s\S]*$/;
+  var botRegex = /[\s\S]*$/;
 
   if(request.text && botRegex.test(request.text)) {
     this.res.writeHead(200);
